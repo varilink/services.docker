6128e7d7e826cd681784335e8b8222ec9e983ffc
David Williamson <david.williamson@varilink.co.uk>
2022-07-02 13:13:23 +0100

    DIG CLIENT AUTOMATICALLY USES CORRECT DNS SERVER BASED ON LOOKUP SCOPE

--- docker/build/dig/docker-entrypoint.sh
+++ docker/build/dig/docker-entrypoint.sh
@@ -2,7 +2,26 @@
 
 set -e
 
-#umount /etc/resolv.conf
-#cp resolv.conf /etc/.
+if [[ ! $@ ]]; then
 
-exec bash
+  echo 'The lookup scope, "external" or "internal" must be provided'
+
+fi
+
+scope=$1
+shift
+
+if [[ "$scope" == 'external' ]]; then
+
+  exec dig @10.0.0.106 $@
+
+elif [[ "$scope" == 'internal' ]]; then
+
+  exec dig @10.0.0.107 $@
+
+else
+
+  echo 'The first parameter must be the lookup scope, "external" or "internal"'
+  exit 1
+
+fi
