50785687b1c1eb60c3b7d23e1e08e22ee50b57ad
David Williamson <david.williamson@varilink.co.uk>
2023-01-20 23:46:13 +0000

    DEFINE THE BUILD CONTEXT FOR A RAISE-HOSTS DOCKER COMPOSE SERVICE
    This encapsulates the creation of the docker and docker-compose commands
    needed to bring an environment up for Ansible deployment within a
    container. I've done this to reduce the dependency on what's installed
    on the clent desktop. The client desktop no longer needs to support bash
    as a mandatory requirement.

--- build/raise-hosts/docker-entrypoint.sh
+++ build/raise-hosts/docker-entrypoint.sh
@@ -1,20 +1,62 @@
-#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# build/hosts/docker-entrypoint.sh
+# ------------------------------------------------------------------------------
 
-source $(dirname "$0")/services.sh
+# This is the entrypoint script for the Docker Compose "hosts" service. It
+# generates the Docker Compose commands to bring up the Docker Compose services
+# that simulate the hosts for an environment. It will limit those hosts to those
+# required by a list of composite services if such a list is passed to it.
 
-services $@
+# ------------------------------------------------------------------------------
 
-docker-compose stop $services
-docker-compose rm --force $services
+# /usr/src/services.sh is mapped to the services.sh provided for the current
+# environment, which is one of "live", "to-be" or "distributed". It sets MYENV
+# to the name of the current environment and provides the services function -
+# see the next comment.
+source /usr/src/services-to-hosts.sh
 
-for service in $services
-do
+# Run the services function provided by /usr/src/services.sh, passing it the
+# list of required composite services. An empty list translates to all composite
+# service. This function sets the services variable to the list of Docker
+# Compose services that the composite services map to in this environment.
+services-to-hosts $@
 
-  if [ "$service" == "backup-director" ]
+# Teardown first, so stop and remove containers and remove volumes that are
+# within the scope of the required composite services.
+
+cat << EOF
+docker-compose --env-file envs/$MYENV/.env stop $hosts
+docker-compose --env-file envs/$MYENV/.env rm --force $hosts
+EOF
+
+if [ $# -eq 0 ] || [ `echo "$@" | tr ' ' '\n' | grep backup` ]
+then
+
+  # The backup composite services is included with the scope.
+
+  if [[ $MYENV == 'live' || $MYENV == 'to-be' ]]
   then
-    docker volume rm services_backup
+
+    # The backup_director and backup_storage roles share the same host and
+    # thus the same bacula-home volume.
+
+    echo 'docker volume rm services_bacula-home'
+
+  elif [[ $MYENV == 'distributed' ]]
+  then
+
+    # The backup_director and backup_storage roles are on separate hosts, each
+    # with its own volume mapped to the bacula home directory.
+    echo 'docker volume rm services_dir-bacula-home services_sd-bacula-home'
+
+  else
+
+    exit 1
+
   fi
 
-done
+fi
 
-docker-compose --env-file ./deploy.env up $services
+# Bring up the service containers for this environment and the required Docker
+# Compose services.
+echo "docker-compose --env-file envs/$MYENV/.env up $hosts"
