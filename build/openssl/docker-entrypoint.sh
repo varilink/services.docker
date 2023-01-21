a434410c396ef930814ae2e6a8037da7b2600837
David Williamson <david.williamson@varilink.co.uk>
2023-01-21 08:37:21 +0000

    CHANGES TO THE BUILD CONTEXT FOR THE OPENSSL IMAGE
    Changes as follows:
    
     - Comments in the entrypoing script.
    
     - Pick up the nameserver IP address for whatever is the current
       environment.
    
     - Cater for different website subdomains.
    
     - Don't automatically print out the certificate.
    
     - Allow other openssl options to be passed on the command line.
    
     - Don't require entrypoint script to be directly executable.

--- build/openssl/docker-entrypoint.sh
+++ build/openssl/docker-entrypoint.sh
@@ -1,15 +1,25 @@
-#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# build/openssl/docker-entrypoint.sh
+# ------------------------------------------------------------------------------
 
 set -e
 
+# The first command line argument is mandatory and must be the FQDN of the
+# request; for example imap.customer.com, dev.home.com, etc.
 fqdn="$1"
+shift
 
-cat <<- 'EOF' > /etc/resolv.conf
+# Use our own DNS service not the inbuilt Docker one.
+cat <<- EOF > /etc/resolv.conf
 search home.com
-nameserver 10.0.0.105
+nameserver ${INTERNAL_NAMESERVER}
 options ndots:0
 EOF
 
+# Extract the host name from the FQDN; for example:
+# imap.customer.com -> imap
+# dev.home.com -> dev
+# etc.
 IFS='.' read -a labels <<< "$fqdn"
 for label in "${labels[@]}"; do
   host=$label
@@ -18,19 +28,24 @@ done
 
 case $host in
 
-  imap)
+  'imap')
+
     port=993
     ;;
 
-  smtp)
+  'smtp')
+
     port=465
     ;;
 
-  www)
+  *)
+
+    # If the host name is not imap or smtp then we assume that the request is
+    # for a web service; for example www, dev, test or staging subdomains.
+
     port=443
     ;;
 
 esac
 
-openssl s_client -connect $fqdn:$port -servername $fqdn |                      \
-openssl x509 -noout -text
+openssl s_client -connect $fqdn:$port "$@"
