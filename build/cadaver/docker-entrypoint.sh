3bf3ccc199efa337617b9c22ca1fa03638585b02
David Williamson <david.williamson@varilink.co.uk>
2023-01-30 21:41:49 +0000

    IMPROVEMENTS TO CADAVER CLIENT
     - Remove need to docker-entrypoint.sh to be executable.
    
     - Use environment variables for the home domain and internal DNS server
       IP address in the resolv.conf file rather than hard coding them.

--- build/cadaver/docker-entrypoint.sh
+++ build/cadaver/docker-entrypoint.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
-
 set -e
 
-cat << 'EOF' > /etc/resolv.conf
-search varilink.co.uk
-nameserver 10.0.0.106
+cat << EOF > /etc/resolv.conf
+search ${HOME_DOMAIN}
+nameserver ${INTERNAL_NAMESERVER}
+options ndots:0
 EOF
 
-cadaver http://caldav:5232
+exec cadaver http://caldav:5232
