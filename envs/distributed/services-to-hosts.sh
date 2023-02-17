2ead2a4a93f116238486a380c335b13916a4318e
David Williamson <david.williamson@varilink.co.uk>
2023-02-17 13:43:44 +0000

    AFTER VERY BASIC TESTS OF SERVICES CREATE PLAYBOOK IN DISTRIBUTED ENV
    The "very basic tests" were to run the playbook for each of the services
    and then undertake checks as identified below on a service-by-service
    basis.
    
    Backup:
    Connect to the director using the console client and check the status of
    the director, storage daemon and one of the clients that is up when the
    scope is limited to backup.
    
    Calendar:
    Connect to radicale using the cadaver console client. Note that Radicale
    3 prompts for authentication credentials, despite me thinking that I've
    disable authentication.
    
    DNS:
    Using the dig client accessing the internal DNS service lookup both the
    host of the internal DNS service and some true, external domain. The
    latter test just checks the upstream resolution path through to an
    external DNS provider via the dns-external host.
    
    Dynamic DNS:
    Just confirm a clean run of the Ansible playbook. Without Linode
    integration enabled this service does nothing functionally that could be
    tested.
    
    Mail:
    Just confirm a clean run of the Ansible playbook. Until the project
    playbooks are run there are no domain mail services to test.
    
    Web:
    Just confirm a clean run of the Ansible playbook. Until the project
    playbooks are run there are no websites to test.

--- envs/distributed/services-to-hosts.sh
+++ envs/distributed/services-to-hosts.sh
@@ -1,60 +1,45 @@
-# Provides the function services that translates composite services into Docker
-# Compose services. This function can be passed zero or more composite services
-# from this list:
-# - backup
-# - calendar
-# - dns
-# - dynamic_dns
-# - mail
-# - web
-# It sets a variable "services" to a sorted list of either all the Docker
-# Compose services (if no composite services were passed) or all the Docker
-# Compose services associated with the passed composite services.
+# ------------------------------------------------------------------------------
+# envs/distributed/services-to-hosts.sh
+# ------------------------------------------------------------------------------
 
-function services {
+function services-to-hosts {
 
-  # Docker Compose services that combine to deliver the backup composite
-  # service.
-  backup='backup-director database-internal dns-external dns-internal'
-  # Docker Compose services that combine to deliver the calendar composite
-  # service.
+  backup='backup-director backup-storage database-internal'
+  backup="$backup dns-external dns-internal mail-internal"
   calendar='caldav dns-external dns-internal'
-  # Docker Compose services that combine to deliver the dns composite service.
   dns='dns-external dns-internal'
-  # Docker Compose services that combine to deliver the dynamic_dns composite
-  # service.
   dynamic_dns='dns-external dns-internal dynamic-dns mail-internal'
-  # Docker Compose services that combine to deliver the mail composite service.
   mail='dns-external dns-internal'
-  mail="$mail mail-certificates mail-external mail-internal mail-other"
-  # Docker Compose services that combine to deliver the web composite service.
-  web='database-external reverse-proxy-external wordpress-external'
-  web="$web wordpress-stack dns-external dns-internal"
+  mail="$mail mail-certificates mail-external mail-internal"
+  web='database-external database-internal'
+  web="$web reverse-proxy-external reverse-proxy-gateway reverse-proxy-internal"
+  web="$web wordpress-external wordpress-internal"
+  web="$web dns-external dns-internal"
 
   if [[ $# -eq 0 ]]
   then
 
-    # No parameters were provided so return ALL Docker Compose services.
-    services="$backup $calendar $dns $dynamic_dns $mail $web"
+    hosts="$backup $calendar $dns $dynamic_dns $mail $web"
 
   else
 
-    # One or more composite services were provided so loop through them.
     for service in $@
     do
 
       if [[ 'backup calendar dns dynamic_dns mail web' =~ ( |^)$service( |$) ]]
       then
 
-        # The composite service is one of backup, calendar, dns, dynamic_dns,
-        # mail or web. Append the multiple Docker Compose services that combine
-        # to deliver that composite service to the output.
-        services="$services ${!service}"
+        # Concatenates the content of the variable whose name is $service to the
+        # string $services.
+        hosts="$hosts ${!service}"
 
       else
 
-        # We were passed a composite service name that we do not recognise, so
-        # exit with an error response.
+        # This function is called by scripts, the output of which is expected to
+        # be piped into another shell. That's why echo is repeated in the output
+        # of the echo command below, so that the other shell executes that
+        # second echo.
+        echo "echo \"I do not recognise $service as a valid composite service\""
         exit 1
 
       fi
@@ -63,8 +48,6 @@ function services {
 
   fi
 
-  # Remove any duplicates from the list of Docker Compose services and sort them
-  # alphabetically.
-  services=`echo $services | xargs -n1 | sort | uniq`
+  hosts=`echo $hosts | xargs -n1 | sort | uniq | paste -sd " "`
 
 }
