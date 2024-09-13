3d4b3d040100db5e406b9b34a55799f2137499f7
David Williamson <david.williamson@varilink.co.uk>
2024-09-13 10:06:16 +0100

    ENHANCEMENTS TO THE SWAKS TEST CLIENT
    As follows:
    
     - Make is available for use on both the office and internet Docker
       networks.
    
     - Remove the redundant use of a simulation of a third-party mail
       server, simply connect directly to our mail servers instead.
    
     - Add support for testing of the spam and AV functions on the mail
       server hosted on the internet Docker network.

--- build/swaks/docker-entrypoint.sh
+++ build/swaks/docker-entrypoint.sh
@@ -1,9 +1,49 @@
 set -e
 
-cat << 'EOF' > /etc/resolv.conf
-search ${HOME_DOMAIN}
-nameserver ${INTERNAL_NAMESERVER}
-options ndots:0
-EOF
+echo $2
 
-exec swaks --from root@other.com --to $1 --server mail-other
+ip_address=$(hostname -i)
+
+if [[ $ip_address =~ ^10\.0\.1 ]]
+then
+
+	cat <<- EOF > /etc/resolv.conf
+	domain ${HOME_DOMAIN}
+	nameserver ${EXTERNAL_NAMESERVER}
+	options ndots:0
+	EOF
+
+elif [[ $ip_address =~ ^10\.0\.0 ]]
+then
+
+	cat <<- EOF > /etc/resolv.conf
+	domain ${HOME_DOMAIN}
+	nameserver ${INTERNAL_NAMESERVER}
+	options ndots:0
+	EOF
+
+fi
+
+if [ "$2" = 'spam' ]
+then
+
+    exec swaks --from root@other.com --to $1 --server mail                     \
+        --header 'Subject: GTUBE Test'                                         \
+        --body                                                                 \
+        'XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X'
+
+elif [ "$2" = 'virus' ]
+then
+
+    exec swaks --from root@other.com --to $1 --server mail                     \
+        --header 'EICAR Anti-Virus Test File'                                  \
+        --body 'The EICAR anti-virus test file is attached'                    \
+        --attach-type text/plain --attach-name eicar.com                       \
+        --attach                                                               \
+        'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
+
+else
+
+    exec swaks --from root@other.com --to $1 --server mail
+
+fi
